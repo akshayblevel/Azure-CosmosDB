3ac4b00950d46db274606563e69f3bb8ccba62fc
akshayblevel <akshayblevel@hotmail.com>
2018-09-29 00:24:54 +0530

    First Commit

---
+++ CosmosDB/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmosDB
+{
+    class Program
+    {
+        static string endpointUrl = string.Empty;
+        static string authorizationKey = string.Empty;
+        static DocumentClient ddbClient;
+        static void Main(string[] args)
+        {
+            string db = "RechargeWithPartition";
+            string collection = "Prepaid";
+            endpointUrl = "https://utilitybillpayments.documents.azure.com:443/";
+            authorizationKey = "4zSaBLfVXeEgv2Is6V6s5X6SUQ==";
+
+            try
+            {
+                Transaction txn = new Transaction();
+                for (int i = 0; i < 10; i++)
+                {
+                    txn = txn.GetDefaultEntity(txn);
+
+                    if (ddbClient == null)
+                        ddbClient = new DocumentClient(new Uri(endpointUrl), authorizationKey);
+
+                    ddbClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri("Recharge", "Prepaid"), txn).Wait();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //CALL BELOW METHOD TO CREATE COSMOS DATABASE IF NOT CREATED THROUGH PORTAL
+        static async Task<Database> CreateDatabase(string ddbName)
+        {
+            ddbClient = new DocumentClient(new Uri(endpointUrl), authorizationKey);
+            Database ddbDatabase = ddbClient.CreateDatabaseQuery()
+                            .Where(d => d.Id == ddbName).AsEnumerable().FirstOrDefault();
+            if (ddbDatabase == null)
+            {
+                ddbDatabase = await ddbClient.CreateDatabaseAsync(new Database()
+                {
+                    Id = ddbName
+                });
+
+            }
+            return ddbDatabase;
+        }
+
+        //CALL BELOW METHOD TO CREATE COLLECTION UNDER COSMOS DATABASE IF NOT CREATED THROUGH PORTAL
+        static async void CreateCollection(Database ddb, string colName)
+        {
+            var docCCollection = ddbClient.CreateDocumentCollectionQuery("dbs/" + ddb.Id)
+                        .Where(c => c.Id == colName).AsEnumerable().FirstOrDefault();
+
+            if (docCCollection == null)
+            {
+                docCCollection = await ddbClient.CreateDocumentCollectionAsync("dbs/" + ddb.Id,
+                    new DocumentCollection
+                    {
+                        Id = colName
+                    });
+
+            }
+        }
+    }
+}
---
+++ CosmosDB/Transaction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosDB
+{
+    class Transaction
+    {
+        public string id { get; set; }
+        public string Operator { get; set; }
+        public string Provider { get; set; }
+        public string Region { get; set; }
+        public string Mobile { get; set; }
+        public string Amount { get; set; }
+        public string Status { get; set; }
+        public string ExecutionTime { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        string[] Operators = new string[] { "JIO", "Vodafone", "Idea","Airtel" };
+        string[] Providers = new string[] { "Euronet", "CyberPlat", "Paytm" };
+        string[] Regions = new string[] { "Gujarat", "Maharashtra", "Telengana" };
+        string[] Mobiles = new string[] { "9999999999", "8888888888", "7777777777" };
+        string[] Amounts = new string[] { "50", "100", "200","500" };
+        string[] ExecutionTimes = new string[] { "100 ms", "200 ms", "500 ms" };
+        string[] Statuses = new string[] { "Fail", "Success", "Pending"};
+        public Transaction()
+        {
+            
+        }
+
+        public Transaction GetDefaultEntity(Transaction txn)
+        {
+            Random random = new Random();
+            txn.id = Convert.ToString(Guid.NewGuid());
+            txn.Operator = Operators[random.Next(Operators.Length)];
+            txn.Provider = Providers[random.Next(Providers.Length)];
+            txn.Region = Regions[random.Next(Regions.Length)];
+            txn.Mobile = Mobiles[random.Next(Mobiles.Length)];
+            txn.Amount = Amounts[random.Next(Amounts.Length)];
+            txn.Status = Statuses[random.Next(Statuses.Length)];
+            txn.ExecutionTime = ExecutionTimes[random.Next(ExecutionTimes.Length)];
+            txn.CreatedDate = DateTime.Now;
+
+            return txn;
+        }
+
+    }
+}
